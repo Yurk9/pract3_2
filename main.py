9522f61c3b8586a1200bf3face0062c9702eb9de
gr622_yurnal <nikitaurin56@gmail.com>
2025-05-30 16:42:50 +0700

    Первая функция

---
+++ main.py
@@ -0,0 +1,9 @@
+import random
+
+def fake_news_generator():
+    subjects = ["Ученые", "Кошки", "Правительство", "Инопланетяне", "ИИ ChatGPT"]
+    actions = ["скрывают", "обнаружили", "создали", "взломали", "съели"]
+    objects = ["эликсир бессмертия", "секретную теорию заговора", "новый вид энергии", "доказательство плоской Земли"]
+    return f"{random.choice(subjects)} {random.choice(actions)} {random.choice(objects)}!"
+
+print(fake_news_generator())  # Например: "Инопланетяне съели эликсир бессмертия!"
