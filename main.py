7a0548d765c777e0b840a3e981ff26f124ca3b01
gr622_yurnal <nikitaurin56@gmail.com>
2025-05-30 16:43:59 +0700

    Вторая функция

---
+++ main.py
@@ -0,0 +1,6 @@
+def is_dozhd(text):
+    rain_words = {"дождь", "листья", "осень", "метель", "память", "электричка"}
+    return any(word in text.lower() for word in rain_words)
+
+print(is_dozhd("За окном снова дождь"))  # True
+print(is_dozhd("Солнце светит ярко"))    # False
